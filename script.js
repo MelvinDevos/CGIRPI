10b4319aa48aa0d5e90edfebaf6c73d474a13035
Melvin Devos <melvin.devos@student.vives.be>
2021-10-18 16:05:10 +0100

    Basis done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Opgestart");
+  var index_table = 0;
+  var json_table = document.getElementById("table-body");
 
   fetch("http://rpi-Melvin/CGIRPI/file.json")
     .then((response) => {
@@ -12,15 +14,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
       response.json().then((data) => {
         // Voor elk datapunt in array table row aanmaken
+
         data.forEach((i) => {
           console.log(i);
-          document.getElementById(
-            "table-body"
-          ).innerHTML += `<tr> <td>${i.id}</td> <td>${i.name}</td> <td>${i.submit_time}</td></tr>`;
+          addToTable(json_table, index_table++, i.name, i.submit_date);
         });
       });
     })
     .catch((error) => {
       console.log("Error bij fetch:", error.message);
     });
+
+  document.forms["form-id"].addEventListener("submit", (event) => {
+    event.preventDefault();
+    // TODO do something here to show user that form is being submitted
+    fetch(event.target.action, {
+      method: "POST",
+      body: new URLSearchParams(new FormData(event.target)), // event.target is the form
+    })
+      .then((resp) => {
+        return resp.json(); // or resp.text() or whatever the server sends
+      })
+      .then((body) => {
+        // TODO handle body
+        console.log(body);
+        addToTable(json_table, index_table++, body.name, body.submit_date)
+      })
+      .catch((error) => {
+        // TODO handle error
+        console.log(error);
+      });
+  });
+
+  function addToTable(table, index, name, date) {
+    table.innerHTML += `<tr> <td>${index}</td> <td>${name}</td> <td>${date}</td></tr>`;
+  }
 });
